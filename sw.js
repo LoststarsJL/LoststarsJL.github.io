c4b5173b215df923fb57803bcad7a1024f9061e9
Leon <1615712969@qq.com>
2023-03-18 23:12:04 +0800

    修改图片过大问题

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ const PRECACHE_LIST = [
   "./js/snackbar.js",
   // "./img/icon_wechat.png",
   "./img/cat.jpg",
-  "./img/home-bg.png",
+  "./img/home-bg.jpg",
   "./img/404-bg.jpg",
   "./css/hux-blog.min.css",
   "./css/bootstrap.min.css"
